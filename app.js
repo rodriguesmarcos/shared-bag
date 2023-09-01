3d61f9b642fd14eed115fbb3a58efeb7a19bee8d
Marcos Rodrigues <eu@marcosrodrigues.me>
2023-09-01 18:52:37 +0000

    api socket

--- app.js
+++ app.js
@@ -1,13 +1,47 @@
+// Import required modules
 const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+
+// Create an Express app
 const app = express();
-const port = process.env.PORT ?? 3000;
+const server = http.createServer(app);
 
-app.use(express.static('public'))
+// Initialize Socket.io
+const io = socketIo(server);
 
-app.get('*', (req, res) => {
-    res.redirect('/');
-})
+// Enable CORS for all routes
+app.use(cors());
 
-app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
-})
+// Serve static files (e.g., HTML, CSS, JS)
+app.use(express.static(__dirname + '/public'));
+
+// Define a route for the home page
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+// Socket.io connection handling
+io.on('connection', (socket) => {
+  console.log('A user connected', socket.id);
+
+//   console.log();
+
+  // Handle chat messages
+  socket.on('chat message', (msg) => {
+    // Broadcast the message to all connected clients
+    io.emit('chat message', msg);
+  });
+
+  // Handle disconnection
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
